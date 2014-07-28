8c9a5fed83bccfbacdddaa3b531cb1c2cf0dc518
marcoo <marcoo@infosupport.com>
2014-07-28 20:44:28 +0200

    Added NameConverter and unit tests

---
+++ Storm.Test/NameConverters/DefaultNameConverterTest.cs
@@ -0,0 +1,84 @@
+﻿using Flyingpie.Storm.NameConverters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Flyingpie.Storm.Test.NameConverters
+{
+    [TestClass]
+    public class DefaultNameConverterTest
+    {
+        private INameConverter _nameConverter;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _nameConverter = new DefaultNameConverter();
+        }
+
+        [TestMethod]
+        public void ConvertSchemaToInterfaceTest()
+        {
+            Assert.AreEqual("IDbo", _nameConverter.ConvertSchemaToInterface("dbo"));
+            Assert.AreEqual("IOrm", _nameConverter.ConvertSchemaToInterface("orm"));
+            Assert.AreEqual("IA", _nameConverter.ConvertSchemaToInterface("a"));
+            Assert.AreEqual("ISchemaName", _nameConverter.ConvertSchemaToInterface("SchemaName"));
+        }
+
+        [TestMethod]
+        public void ConvertSchemaToClassTest()
+        {
+            Assert.AreEqual("Dbo", _nameConverter.ConvertSchemaToClass("dbo"));
+            Assert.AreEqual("Orm", _nameConverter.ConvertSchemaToClass("orm"));
+            Assert.AreEqual("A", _nameConverter.ConvertSchemaToClass("a"));
+            Assert.AreEqual("SchemaName", _nameConverter.ConvertSchemaToClass("SchemaName"));
+        }
+
+        [TestMethod]
+        public void ConvertStoredProcedureToMethodTest()
+        {
+            Assert.AreEqual("GetVendors", _nameConverter.ConvertStoredProcedureToMethod("get_vendors"));
+            Assert.AreEqual("Orm", _nameConverter.ConvertStoredProcedureToMethod("orm"));
+            Assert.AreEqual("A", _nameConverter.ConvertStoredProcedureToMethod("a"));
+            Assert.AreEqual("AddProduct", _nameConverter.ConvertStoredProcedureToMethod("addProduct"));
+        }
+
+        [TestMethod]
+        public void ConvertParameterTest()
+        {
+            Assert.AreEqual("id", _nameConverter.ConvertParameter("@ID"));
+            Assert.AreEqual("someId", _nameConverter.ConvertParameter("@SomeID"));
+            Assert.AreEqual("productName", _nameConverter.ConvertParameter("@PRODUCT_NAME"));
+            Assert.AreEqual("productName", _nameConverter.ConvertParameter("@ProductName"));
+            Assert.AreEqual("productName", _nameConverter.ConvertParameter("@product_name"));
+            Assert.AreEqual("productName", _nameConverter.ConvertParameter("@product__name"));
+        }
+
+        [TestMethod]
+        public void ConvertColumnToPropertyTest()
+        {
+            Assert.AreEqual("VendorName", _nameConverter.ConvertColumnToProperty("vendor_name"));
+            Assert.AreEqual("Orm", _nameConverter.ConvertColumnToProperty("orm"));
+            Assert.AreEqual("A", _nameConverter.ConvertColumnToProperty("a"));
+            Assert.AreEqual("ProductName", _nameConverter.ConvertColumnToProperty("PRODUCT_NAME"));
+            Assert.AreEqual("ProductName", _nameConverter.ConvertColumnToProperty("ProductName"));
+            Assert.AreEqual("ProductName", _nameConverter.ConvertColumnToProperty("product_name"));
+            Assert.AreEqual("ProductName", _nameConverter.ConvertColumnToProperty("product__name"));
+        }
+
+        [TestMethod]
+        public void ConvertUserDefinedTypeToClassTest()
+        {
+            Assert.AreEqual("Dbo", _nameConverter.ConvertSchemaToClass("dbo"));
+            Assert.AreEqual("Orm", _nameConverter.ConvertSchemaToClass("orm"));
+            Assert.AreEqual("A", _nameConverter.ConvertSchemaToClass("a"));
+            Assert.AreEqual("UdtName", _nameConverter.ConvertSchemaToClass("udtName"));
+            Assert.AreEqual("ProductName", _nameConverter.ConvertColumnToProperty("PRODUCT_NAME"));
+            Assert.AreEqual("ProductName", _nameConverter.ConvertColumnToProperty("ProductName"));
+            Assert.AreEqual("ProductName", _nameConverter.ConvertColumnToProperty("product_name"));
+            Assert.AreEqual("ProductName", _nameConverter.ConvertColumnToProperty("product__name"));
+        }
+    }
+}
--- Storm/NameConverters/DefaultNameConverter.cs
+++ Storm/NameConverters/DefaultNameConverter.cs
@@ -32,6 +32,10 @@ namespace Flyingpie.Storm.NameConverters
         {
             name = name.Replace("@", "");
 
+            name = Capitalize(name);
+
+            name = name.Substring(0, 1).ToLower() + (name.Length > 1 ? name.Substring(1) : string.Empty);
+
             return name;
         }
 
@@ -51,6 +55,48 @@ namespace Flyingpie.Storm.NameConverters
 
         private string Capitalize(string value)
         {
+            var sb = new StringBuilder();
+
+            var capitalizeNext = false;
+            var lastWasUpper = false;
+            for (int i = 0; i < value.Length; i++)
+            {
+                var c = value[i];
+
+                var isUpper = char.IsUpper(c);
+
+                // If the last character was upper-case and this one is upper-case as well, make this one lower case
+                // AA -> Aa
+                if (lastWasUpper && isUpper)
+                {
+                    c = char.ToLower(c);
+                }
+
+                // If this character is an underscore, keep that in the back of our heads and move on
+                if (c == '_')
+                {
+                    capitalizeNext = true;
+                    continue;
+                }
+
+                // Capitalize if neccessary
+                // aa -> aA
+                if (capitalizeNext)
+                {
+                    sb.Append(c.ToString().ToUpper());
+                    capitalizeNext = false;
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+
+                lastWasUpper = isUpper;
+            }
+
+            value = sb.ToString();
+
+            // Make the first character a capital and append the rest of the string, if it is longer than 1 character
             return value[0].ToString().ToUpper() + (value.Length > 1 ? value.Substring(1) : string.Empty);
         }
     }
