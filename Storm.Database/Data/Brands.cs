073104ba92618ec3ed0d00f893155393fec7953b
marcoo <marcoo@infosupport.com>
2014-08-31 11:43:02 +0200

    Added version.txt Added Migration classes

---
+++ Storm.Database/Data/Brands.cs
@@ -0,0 +1,46 @@
+﻿using FluentMigrator;
+using Flyingpie.Storm.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Storm.Database.Data
+{
+    [Profile("TestData")]
+    public class Brands : Migration
+    {
+        class Brand
+        {
+            public string Name { get; set; }
+
+            public string Description { get; set; }
+
+            public int HorsePower { get; set; }
+        }
+
+        public override void Up()
+        {
+            Insert
+                .IntoTable(Constants.Tables.BRANDS)
+                .InSchema(Constants.Schemas.CARS)
+
+                .Row(new Brand() { Name = "Audi", Description = "The steering wheel is an option", HorsePower = 160 })
+                .Row(new Brand() { Name = "Bentley", Description = null, HorsePower = 200 })
+                .Row(new Brand() { Name = "Hyundai", Description = "Too bad they didn't come in 14%", HorsePower = 140 })
+                .Row(new Brand() { Name = "Lexus", Description = "You get your own mechanic with this one", HorsePower = 125 })
+                .Row(new Brand() { Name = "Mercedes", Description = "Only for the elite", HorsePower = 250 })
+                .Row(new Brand() { Name = "Seat", Description = "Spanish and spicy", HorsePower = 90 })
+                .Row(new Brand() { Name = "Skoda", Description = "", HorsePower = 100 })
+                .Row(new Brand() { Name = "Toyota", Description = "Xander was here", HorsePower = 110 })
+                .Row(new Brand() { Name = "Volkswagen", Description = "Nice, but pricy", HorsePower = 120 })
+                .Row(new Brand() { Name = "Volvo", Description = "The search for perfection ends here", HorsePower = 120 })
+            ;
+        }
+
+        public override void Down()
+        {
+
+        }
+    }
+}
---
+++ Storm.Database/Migrations/0001_Tables.cs
@@ -0,0 +1,63 @@
+﻿using FluentMigrator;
+using Flyingpie.Storm.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Storm.Database.Migrations
+{
+    [Migration(1, "Tables")]
+    public class InitialMigration : Migration
+    {
+        public override void Up()
+        {
+            Create.Schema(Constants.Schemas.CARS);
+            Create.Schema(Constants.Schemas.CUSTOMERS);
+            Create.Schema(Constants.Schemas.UTILITY);
+
+            Create
+                .Table(Constants.Tables.BRANDS)
+                .InSchema(Constants.Schemas.CARS)
+
+                .WithColumn("Id")
+                .AsInt32()
+                .PrimaryKey()
+                .Identity()
+
+                .WithColumn("Name")
+                .AsString()
+                .NotNullable()
+
+                .WithColumn("Description")
+                .AsString()
+                .Nullable()
+
+                .WithColumn("HorsePower")
+                .AsInt32()
+                .NotNullable()
+            ;
+
+            Create
+                .Table(Constants.Tables.MODELS)
+                .InSchema(Constants.Schemas.CARS)
+
+                .WithColumn("Id")
+                .AsInt32()
+                .PrimaryKey()
+                .Identity()
+
+                .WithColumn("Name")
+                .AsString()
+                .NotNullable()
+                
+                .WithColumn("BrandId")
+                .AsInt32()
+            ;
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
