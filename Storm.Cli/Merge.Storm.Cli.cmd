6d42448dc17900d129df96b54eb291c402cdbaa5
marcoo <marcoo@infosupport.com>
2014-09-23 17:12:42 +0200

    Updated Storm.Cli merge script

---
+++ Storm.Cli/Merge.Storm.Cli.cmd
@@ -0,0 +1,11 @@
+﻿ECHO Create a single dll to add in the NuGet package
+
+IF EXIST "Merged.Storm.Cli" rmdir /s /q "Merged.Storm.Cli"
+mkdir "Merged.Storm.Cli"
+
+ILRepack.exe /target:exe /internalize /out:"Merged.Storm.Cli/stormcli.exe" ^
+  Flyingpie.Storm.Cli.exe ^
+  CommandLine.dll ^
+  Flyingpie.Storm.dll ^
+  Flyingpie.Storm.Dapper.dll ^
+  Dapper.dll
